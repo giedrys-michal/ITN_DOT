95d1760314ea120c2f77547af0e5c72c4fe7d56e
giedrys-michal <giedrys.michal@gmail.com>
2020-03-22 14:51:09 +0100

    Utworzenie formularza, pola gry, kolorowanie rzędów.

---
+++ Gra/Gra/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Gra
+{
+    partial class frmPrzeszkody
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlPanel = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnlPanel
+            // 
+            this.pnlPanel.Location = new System.Drawing.Point(5, 2);
+            this.pnlPanel.Name = "pnlPanel";
+            this.pnlPanel.Size = new System.Drawing.Size(510, 410);
+            this.pnlPanel.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 427);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(147, 436);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(35, 13);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "CZAS";
+            // 
+            // frmPrzeszkody
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 471);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmPrzeszkody";
+            this.Text = "Przeszkody";
+            this.Load += new System.EventHandler(this.frmPrzeszkody_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPanel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ Gra/Gra/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gra
+{
+    public partial class frmPrzeszkody : Form
+    {
+        Control.ControlCollection pola;
+        Color kolorPrzeszkody = Color.DarkGray;
+        Random losowe = new Random();
+        bool kolej = true;
+
+
+        public frmPrzeszkody()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrzeszkody_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    TextBox pole = new TextBox
+                    {
+                        Width = 70,
+                        Left = j * 75 + 10,
+                        Top = i * 30 + 10,
+                        Enabled = false
+                    };
+                    this.pnlPanel.Controls.Add(pole);
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            pola = this.pnlPanel.Controls;
+
+            for (int i = 54; i > 4; i--)
+            {
+                pola[i].BackColor = pola[i - 5].BackColor;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                pola[i].BackColor = SystemColors.Window;
+            }
+
+            if (kolej)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    pola[losowe.Next(0, 5)].BackColor = kolorPrzeszkody;
+                }
+            }
+            kolej = !kolej;
+        }
+    }
+}
