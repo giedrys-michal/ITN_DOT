fdf962ede33631542526b1fee411b349633ff0e0
giedrys-michal <giedrys.michal@gmail.com>
2020-05-08 21:34:29 +0200

    Pierwsza iteracja MVC v1

---
+++ MVC5/MVC_v1/MVC_v1/Controllers/AukcjeController.cs
@@ -0,0 +1,40 @@
+﻿using MVC_v1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_v1.Controllers
+{
+    public class AukcjeController : Controller
+    {
+        // GET: Aukcje
+
+        public ActionResult Index(string nazwa)
+        {
+            List<Aukcja> aukcje = new List<Aukcja>()
+            {
+                new Aukcja("Latarka", 12.99m, "Zawisza Czarny", 14),
+                new Aukcja("Mlotek", 8, "Janusz", 7),
+                new Aukcja("Lampka", 94.99m, "Mirek", 7),
+            };
+
+            aukcje[1].status = Status.anulowana;
+            aukcje[2].ceny.Add(120);
+            aukcje[2].ceny.Add(130);
+            aukcje[2].ceny.Add(150);
+            aukcje[2].ceny.Add(180);
+
+
+            // na niepustej kolekcji wywal aukcje których nazwa zawiera to co w filtrze
+            if (!String.IsNullOrEmpty(nazwa))
+                for (int i = 0; i < aukcje.Count; i++)
+                    if (!aukcje[i].przedmiot.Contains(nazwa))
+                        aukcje.RemoveAt(i--);
+
+            return View(aukcje);
+        }
+    }
+}
---
+++ MVC5/MVC_v1/MVC_v1/Models/Aukcja.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_v1.Models
+{
+    public enum Status { aktywna, wstrzymana, anulowana, oczekuje };
+    public class Aukcja
+    {
+        public static int liczbaAukcji;
+        public int aukcjaID { get; set; }
+        public string przedmiot { get; set; }
+        public decimal cenaWywolawcza { get; set; }
+        public List<decimal> ceny = new List<decimal>();
+        public Status status { get; set; }
+        public DateTime dataStart { get; set; }
+        public DateTime dataZakonczenia { get; set; }
+        public string sprzedajacy { get; set; }
+
+        public Aukcja(string przedmiot, decimal cenaWywolawcza, string sprzedajacy, double czasTrwania)
+        {
+            this.aukcjaID = ++liczbaAukcji;
+            this.przedmiot = przedmiot;
+            this.cenaWywolawcza = cenaWywolawcza;
+            this.dataStart = DateTime.Now;
+            this.dataZakonczenia = DateTime.Now.AddDays(czasTrwania);
+            this.status = Status.aktywna;
+            this.sprzedajacy = sprzedajacy;
+        }
+    }
+}
